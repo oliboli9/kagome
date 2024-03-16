6aa9314d178a54c8d924228446822fd69f8f5b11
Oliwia Pindel <s215142@dtu.dk>
2024-03-16 19:13:40 +0100

    Add surfaces

---
+++ surface.py
@@ -0,0 +1,71 @@
+import math
+import numpy as np
+from scipy.integrate import dblquad
+import sympy as sp
+
+class Surface:
+    def __init__(self, f, density=None, n=None):
+        x, y = sp.symbols("x y")
+        self.f = f(x, y)
+        self.density = density
+        self.df_dx = sp.diff(self.f, x)
+        self.df_dy = sp.diff(self.f, y)
+        """
+        The number of atoms n is such that the density of atoms on the surface is 1/9 atoms/Å^2
+        (optimal density is based on # of Au atoms that make a lattice with Au bond distance)
+        """
+        area_expr = sp.lambdify(
+            (x, y), sp.sqrt(1 + self.df_dx**2 + self.df_dy**2), "numpy"
+        )
+        area, error = dblquad(area_expr, 0, 30, lambda x: 0, lambda x: 30)
+        if n is None:
+            assert (
+                density is not None
+            ), "Pass either desired density or desired number of atoms"
+            self.n = int(area / density)
+        else:
+            self.n = n
+            self.density = area / n
+
+    def elevate_to_surface(self, x_vals, y_vals):
+        x, y = sp.symbols("x y")
+        f_num = sp.lambdify((x, y), self.f, "numpy")
+        return f_num(x_vals, y_vals)
+
+
+    def dx(self, x_vals, y_vals):
+        x, y = sp.symbols("x y")
+        df_dx_num = sp.lambdify((x, y), self.df_dx, "numpy")
+        return df_dx_num(x_vals, y_vals)
+
+    def dy(self, x_vals, y_vals):
+        x, y = sp.symbols("x y")
+        df_dy_num = sp.lambdify((x, y), self.df_dy, "numpy")
+        return df_dy_num(x_vals, y_vals)
+
+class SurfaceConstraint:
+    def __init__(self, surface):
+        self.surface: Surface = surface
+
+    def adjust_positions(self, atoms, newpositions):
+        x = newpositions[:, 0]
+        y = newpositions[:, 1]
+
+        z = self.surface.elevate_to_surface(x, y)
+
+        newpositions[:, 2] = z
+
+    def adjust_forces(self, atoms, forces):
+        # Modify the forces to be tangential to the surface
+        positions = atoms.get_positions()
+        x = positions[:, 0]
+        y = positions[:, 1]
+
+        dz_dx = self.surface.dx(x, y)
+        dz_dy = self.surface.dy(x, y)
+        dz_dz = -1 * np.ones(self.surface.n)
+
+        normals = np.stack((dz_dx, dz_dy, dz_dz), axis=-1)
+        normals /= np.linalg.norm(normals, axis=1).reshape(-1, 1)
+        # forces -= np.dot(forces, normals) * normals
+        forces -= np.einsum("ij,ij->ij", forces, normals) * normals
